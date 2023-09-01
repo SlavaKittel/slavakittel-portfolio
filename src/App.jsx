321b3b0824780ac27877c812159b943a67b2e27d
SlavaKittel <slavakittel@gmail.com>
2023-09-01 16:37:05 +0300

    main: copy project

---
+++ src/App.jsx
@@ -0,0 +1,203 @@
+import React, { useEffect, useRef, createRef, useMemo, useState } from "react";
+import './style.css'
+import { StrictMode } from 'react'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+
+import {
+    CuboidCollider,
+    CylinderCollider,
+    Physics,
+    RigidBody,
+    useBeforePhysicsStep,
+} from '@react-three/rapier'
+
+import Car from './Car'
+import * as THREE from 'three'
+import { Leva } from 'leva'
+import { useControls } from './hooks/use-controls'
+import {
+    OrbitControls,
+  } from '@react-three/drei'
+
+
+  const Portfolio = () => {
+    const raycastVehicle = useRef()
+    const controls = useControls()
+
+    const maxForce = 100
+    const maxBrake = 1
+    const maxSteer = 0.7
+
+    // start logic for smooth turning of the front wheels
+    const stepSteer = 0.01
+    let steer = 0;
+    function steerIncreaseLeft() {
+        setTimeout(function() {
+            if (steer < maxSteer && controls.current.left) {
+                steer += stepSteer;
+            }                    
+        }, 70)
+    }
+    function steerDicreaseLeft() {
+        setTimeout(function() {
+            if (steer > stepSteer && !controls.current.left) {
+                steer -= stepSteer;
+            }                                          
+        }, 70)
+    }
+    function steerIncreaseRight() {
+        setTimeout(function() {
+            if (steer > -maxSteer && controls.current.right) {
+                steer -= stepSteer;
+            }                    
+        }, 70)
+    }
+    function steerDicreaseRight() {
+        setTimeout(function() {
+            if (steer < -stepSteer  && !controls.current.right) {
+                steer += stepSteer;
+            }                                          
+        }, 70)
+    }
+    // end logic for smooth turning of the front wheels
+
+    useBeforePhysicsStep((world) => {
+        if (
+            !raycastVehicle.current ||
+            !raycastVehicle.current.rapierRaycastVehicle.current
+        ) {
+            return
+        }
+
+
+        const {
+            wheels,
+            rapierRaycastVehicle: { current: vehicle },
+            setBraking,
+        } = raycastVehicle.current
+
+        // update wheels from controls
+        let engineForce = 0
+        let steering = 0
+
+        if (controls.current.forward) {
+            engineForce += maxForce
+        }
+        if (controls.current.backward) {
+            engineForce -= maxForce
+        }
+
+        console.log(steering)
+        console.log(steer)
+
+        if (controls.current.left) {
+            steerIncreaseLeft()
+            steering = steer
+        }
+        if (!controls.current.left) {
+            steerDicreaseLeft()
+            steering = steer
+        }
+        if (controls.current.right) {
+            steerIncreaseRight()
+            steering = steer
+        }
+        if (!controls.current.right) {
+            steerDicreaseRight()
+            steering = steer
+        }
+
+        const brakeForce = controls.current.brake ? maxBrake : 0
+
+        for (let i = 0; i < vehicle.wheels.length; i++) {
+            vehicle.setBrakeValue(brakeForce, i)
+        }
+
+        // steer front wheels
+        vehicle.setSteeringValue(steering, 0)
+        vehicle.setSteeringValue(steering, 1)
+
+        // apply engine force to back wheels
+        vehicle.applyEngineForce(engineForce, 2)
+        vehicle.applyEngineForce(engineForce, 3)
+
+        // update the vehicle
+        vehicle.update(world.timestep)
+
+        // update the wheels
+        for (let i = 0; i < vehicle.wheels.length; i++) {
+            const wheelObject = wheels[i].object.current
+            if (!wheelObject) continue
+
+            const wheelState = vehicle.wheels[i].state
+            wheelObject.position.copy(wheelState.worldTransform.position)
+            wheelObject.quaternion.copy(wheelState.worldTransform.quaternion)
+        }
+
+        // update speed text
+        // if (currentSpeedTextDiv.current) {
+        //     const km = Math.abs(
+        //         vehicle.state.currentVehicleSpeedKmHour
+        //     ).toFixed()
+        //     currentSpeedTextDiv.current.innerText = `${km} km/h`
+        // }
+
+        // update brake lights
+        setBraking(brakeForce > 0)
+    })
+
+    return (
+      <Car
+        ref={raycastVehicle}
+      />
+    )
+}
+
+export default function App() {
+  return (
+    <Canvas
+        dpr ={ [ 1, 2 ] } // default pixelRatio
+        camera={{
+          position: [10, 5, 15],
+          fov: 35, 
+          zoom: 0.5,
+        }}
+        shadows
+    >
+      <Physics
+          debug
+          timeStep="vary"
+      >
+          {/* <Leva collapsed /> */}
+          {/* Camera */}
+          <OrbitControls makeDefault />
+
+          {/* Lights */}
+          <directionalLight castShadow position={[10, 4, 3]} intensity={20} />
+          <ambientLight intensity={5.9} />
+
+          <Portfolio />
+
+          {/* ground */}
+          <RigidBody type='fixed' restitution={1} friction={0.3}>  
+              <mesh 
+                  receiveShadow
+                  position-y={-1}
+              >
+                  <boxGeometry args={[ 200, 0.5, 200]}/>
+                  <meshStandardMaterial color="#f0f0f0" />
+                  {/* <MeshReflectorMaterial
+                  resolution={ 1018 }
+                  blur={ [1000, 1000] }
+                  mixBlur={1}
+                  mirror={ 0 }
+                  color="grey"
+                  /> */}
+              </mesh>
+          </RigidBody>
+          <gridHelper args={[50, 50]} position-y={-0.74} />
+      </Physics>
+    </Canvas>
+  )
+}
---
+++ src/Car.jsx
@@ -0,0 +1,341 @@
+import React, {
+  useEffect,
+  useRef,
+  createRef,
+  useMemo,
+  useImperativeHandle,
+  forwardRef,
+} from "react";
+import { useControls as useLeva } from 'leva'
+import {
+  useGLTF,
+  KeyboardControls,
+  OrbitControls,
+  useKeyboardControls,
+} from "@react-three/drei";
+import {
+  Vector3,
+} from 'three'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { useLoader } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
+import { Perf } from "r3f-perf";
+import {
+  InstancedRigidBodies,
+  CuboidCollider,
+  RigidBody,
+  useRapier,
+  Physics,
+  CylinderCollider,
+  useRevoluteJoint,
+  useFixedJoint,
+  useBeforePhysicsStep,
+} from "@react-three/rapier";
+import {
+  RapierRaycastVehicle,
+} from './lib/rapier-raycast-vehicle'
+
+import WheelURl from "../public/bmwe30wheel2.glb?url";
+
+
+const Wheel = ({ side, rotation, radius }) => {
+  const { nodes, materials } = useGLTF(WheelURl);
+  const scale = radius * 4
+  return (
+    <group dispose={null}>
+      <group scale={scale}>
+        <group scale={side === "left" ? -1 : 1}>
+          <mesh
+            rotation={rotation}
+            radius={radius}
+            castShadow
+            receiveShadow
+            geometry={nodes.wheel_FR_bbs_tex002_0.geometry}
+            material={materials["disk.002"]}
+          />
+          <mesh
+            rotation={rotation}
+            radius={radius}
+            castShadow
+            receiveShadow
+            geometry={nodes.wheel_FR_blt002_0.geometry}
+            material={materials["disk_brake.001"]}
+          />
+          <mesh
+            rotation={rotation}
+            radius={radius}
+            castShadow
+            receiveShadow
+            geometry={nodes.wheel_FR_disk002_0.geometry}
+            material={materials["disk_brake_tr.001"]}
+          />
+          <mesh
+            rotation={rotation}
+            radius={radius}
+            castShadow
+            receiveShadow
+            geometry={nodes.wheel_FR_disk_paint002_0.geometry}
+            material={materials["disk_paint.002"]}
+          />
+          <mesh
+            rotation={rotation}
+            radius={radius}
+            castShadow
+            receiveShadow
+            geometry={nodes.wheel_FR_tire002_0.geometry}
+            material={materials["tire.002"]}
+          />
+        </group>
+      </group>
+    </group>
+  );
+};
+
+
+const Car = forwardRef(function Car({ children },ref) {
+  const { perfVisible } = useLeva({
+    perfVisible: false,
+  });
+
+  const rapier = useRapier()
+
+  const vehicleRef = useRef()
+  const chassisRigidBodyRef = useRef();
+
+  const topLeftWheelObject = useRef()
+  const topRightWheelObject = useRef()
+  const bottomLeftWheelObject = useRef()
+  const bottomRightWheelObject = useRef()
+
+  const bmwE30Chassis = useLoader(GLTFLoader, "./bmwe30withoutwheel.glb");
+
+  const {
+    indexRightAxis,
+    indexForwardAxis,
+    indexUpAxis,
+    directionLocal: directionLocalArray,
+    axleLocal: axleLocalArray,
+    vehicleWidth,
+    vehicleHeight,
+    vehicleFront,
+    vehicleBack,
+    ...levaWheelOptions
+} = useLeva(`wheel-options`, {
+    radius: 0.38,
+
+    indexRightAxis: 2,
+    indexForwardAxis: 0,
+    indexUpAxis: 1,
+
+    directionLocal: [0, -1, 0],
+    axleLocal: [0, 0, 1],
+
+    suspensionStiffness: 30,
+    suspensionRestLength: 0.3,
+    maxSuspensionForce: 100000,
+    maxSuspensionTravel: 0.3,
+
+    sideFrictionStiffness: 1,
+    frictionSlip: 1.4,
+    dampingRelaxation: 2.3,
+    dampingCompression: 4.4,
+
+    rollInfluence: 0.01,
+
+    customSlidingRotationalSpeed: -30,
+    useCustomSlidingRotationalSpeed: true,
+
+    forwardAcceleration: 1,
+    sideAcceleration: 1,
+
+    vehicleWidth: 1.7,
+    vehicleHeight: -0.3,
+    vehicleFront: -1.35,
+    vehicleBack: 1.3,
+})
+
+  // const radius = 0.38
+  // const indexRightAxis = 2
+  // const indexForwardAxis = 0
+  // const indexUpAxis = 1
+  // const directionLocalArray = [0, -1, 0]
+  // const axleLocalArray = [0, 0, 1]
+
+  // const suspensionStiffness = 30
+  // const suspensionRestLength = 0.3
+  // const maxSuspensionForce = 100000
+  // const maxSuspensionTravel = 0.3
+  // const sideFrictionStiffness = 1
+  // const frictionSlip = 1.4
+  // const dampingRelaxation = 2.3
+  // const dampingCompression = 4.4
+  // const rollInfluence = 0.01
+  // const customSlidingRotationalSpeed = -30
+  // const useCustomSlidingRotationalSpeed = true
+  // const forwardAcceleration = 1
+  // const sideAcceleration = 1
+
+  // const vehicleWidth = 1.7
+  // const vehicleHeight = -0.3
+  // const vehicleFront = -1.35
+  // const vehicleBack = 1.3
+
+  const directionLocal = useMemo(
+    () => new Vector3(...directionLocalArray),
+    [directionLocalArray]
+  )
+  const axleLocal = useMemo(
+      () => new Vector3(...axleLocalArray),
+      [axleLocalArray]
+  )
+
+  const commonWheelOptions = {
+      ...levaWheelOptions,
+      directionLocal,
+      axleLocal,
+  }
+
+  const wheels = [
+    {
+        object: topLeftWheelObject,
+        options: {
+            ...commonWheelOptions,
+            chassisConnectionPointLocal: new Vector3(
+                vehicleBack,
+                vehicleHeight,
+                vehicleWidth * 0.5
+            ),
+        },
+    },
+    {
+        object: topRightWheelObject,
+        options: {
+            ...commonWheelOptions,
+            chassisConnectionPointLocal: new Vector3(
+                vehicleBack,
+                vehicleHeight,
+                vehicleWidth * -0.5
+            ),
+        },
+    },
+    {
+        object: bottomLeftWheelObject,
+        options: {
+            ...commonWheelOptions,
+            chassisConnectionPointLocal: new Vector3(
+                vehicleFront,
+                vehicleHeight,
+                vehicleWidth * 0.5
+            ),
+        },
+    },
+    {
+        object: bottomRightWheelObject,
+        options: {
+            ...commonWheelOptions,
+            chassisConnectionPointLocal: new Vector3(
+                vehicleFront,
+                vehicleHeight,
+                vehicleWidth * -0.5
+            ),
+        },
+    },
+  ]
+
+  useImperativeHandle(ref, () => ({
+    chassisRigidBody: chassisRigidBodyRef,
+    rapierRaycastVehicle: vehicleRef,
+    setBraking: () => {
+        // const material = brakeLightsRef.current
+        //     .material as MeshStandardMaterial
+        // material.color = braking
+        //     ? BRAKE_LIGHTS_ON_COLOR
+        //     : BRAKE_LIGHTS_OFF_COLOR
+    },
+    wheels,
+  }))
+
+  useEffect(() => {
+      vehicleRef.current = new RapierRaycastVehicle({
+          world: rapier.world,
+          chassisRigidBody: chassisRigidBodyRef.current,
+          indexRightAxis,
+          indexForwardAxis,
+          indexUpAxis,
+      })
+
+      for (let i = 0; i < wheels.length; i++) {
+          const options = wheels[i].options
+          vehicleRef.current.addWheel(options)
+      }
+
+      vehicleRef.current = vehicleRef.current
+  }, [
+      chassisRigidBodyRef,
+      vehicleRef,
+      indexRightAxis,
+      indexForwardAxis,
+      indexUpAxis,
+      directionLocal,
+      axleLocal,
+      levaWheelOptions,
+  ])
+
+  return (
+    <>
+      {/* {perfVisible && <Perf position="top-left" />} */}
+
+      {/* Chassis */}
+      <RigidBody
+        ref={chassisRigidBodyRef}
+        // colliders="hull"
+        colliders={false}
+        // mass={100} // ??
+        position={[0, 4, 0]}
+        rotation={[0, Math.PI, 0]}
+      >
+        <primitive
+          position={[0, -0.89, 0]}
+          rotation={[0, Math.PI / 2 ,0]}
+          object={ bmwE30Chassis.scene }
+          scale={0.7}
+        />
+        <CuboidCollider args={[2.3, 0.7, 1]} position={[0, 0, 0]} mass={50}/>
+        {children}
+      </RigidBody>
+
+      {/* Wheels */}
+      <group ref={topLeftWheelObject}>
+        <Wheel
+            rotation={[0, -(Math.PI / 2), 0]}
+            side="left"
+            radius={commonWheelOptions.radius}
+        />
+      </group>
+      <group ref={topRightWheelObject}>
+        <Wheel
+            rotation={[0, -(Math.PI / 2), 0]}
+            side="right"
+            radius={commonWheelOptions.radius}
+        />
+      </group>
+      <group ref={bottomLeftWheelObject}>
+        <Wheel
+            rotation={[0, -(Math.PI / 2), 0]}
+            side="left"
+            radius={commonWheelOptions.radius}
+        />
+      </group>
+      <group ref={bottomRightWheelObject}>
+        <Wheel
+            rotation={[0, -(Math.PI / 2), 0]}
+            side="right"
+            radius={commonWheelOptions.radius}
+        />
+      </group>
+  
+    </>
+  );
+});
+
+export default Car;
